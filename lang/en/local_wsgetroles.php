a5ed77dce0ada8f0d132b0f8d4ba9bedd78e53b5
Lucas Hau Dai <lucas.haudai@kineo.com.au>
2022-09-13 15:31:14 +1000

    ICAHAS-291 API to get admin ids

--- lang/en/local_wsgetroles.php
+++ lang/en/local_wsgetroles.php
@@ -1,26 +1,9 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
- * Language file for local_wsgetroles
+ * Language file for local_ws_get_admins
  *
- * @package    local_wsgetroles
- * @copyright  2020 corvus albus
+ * @package    local_ws_get_admins
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['privacy:metadata'] = 'The local_wsgetroles plugin does not store any personal data.';
-$string['pluginname'] = 'Webservice get roles';
+$string['pluginname'] = 'Webservice admins\' id';
